bbf3e6e059dc4ec4dc4f198ce408b3590e0eec7f
nheeww <111355281+nheeww@users.noreply.github.com>
2024-05-02 08:16:10 -0300

    att

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
  const botoes = document.querySelectorA11(".botao");
+ const textos = document.querySelectorA11(".aba-conteudo");
  
  for(let i=0;i,<botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
-            botoes[j].classlist.remove("ativo")
+               botoes[j].classlist.remove("ativo")
+               botoes[j].classlist.remove("ativo")
         }
 
         botoes[i].classlist.add("ativo")
-    }
+        textos{i}.classlist.add("ativo")
      }
